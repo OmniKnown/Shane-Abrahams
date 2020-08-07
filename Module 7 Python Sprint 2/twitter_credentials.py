0edb3eb624d96b037964b0adda5dec335d0411fc
OmniKnown <62374669+OmniKnown@users.noreply.github.com>
2020-08-07 14:29:55 +0200

    Add files via upload

---
+++ Module 7 Python Sprint 2/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter API.
+ACCESS_TOKEN = "1291665636830130176-i1lFpDnaXhXFZkvlqmvHBV5Yliuj67"
+ACCESS_TOKEN_SECRET = "1REaCOGtBj0MVoGXsM5Tn5sPLzi7VnlTmP6rzol7stipo"
+CONSUMER_KEY = "YlDswg62RdB0SkcpxD6vnRcoH"
+CONSUMER_SECRET = "G2khvc51vJqOJtOh7tS1hrwlloc5NCGQlwX3CPru1vKHJxiJcA"
